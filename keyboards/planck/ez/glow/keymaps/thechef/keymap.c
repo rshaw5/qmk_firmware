1ed9a1a5e6381473d5e29575179724e1368d6b1d
Ross Shaw <VicisVitium@gmail.com>
2021-11-12 11:00:36 -0500

    Change layer name to '_NUMPAD'

--- keyboards/planck/ez/glow/keymaps/thechef/keymap.c
+++ keyboards/planck/ez/glow/keymaps/thechef/keymap.c
@@ -63,7 +63,7 @@ enum planck_layers {
   _RAISE,
   _ADJUST,
   _NAVIGATE,
-  _LAYER5,
+  _NUMPAD,
 };
 
 #define LOWER MO(_LOWER)
@@ -106,7 +106,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     TO(0)  , _______, _______, _______, _______, _______, XXXXXXX, _______, _______, _______, _______, _______
   ),
 
-  [_LAYER5] = LAYOUT_planck_grid(
+  [_NUMPAD] = LAYOUT_planck_grid(
     _______, _______, _______, _______, _______, _______, _______, KC_KP_7, KC_KP_8, KC_KP_9, KC_PAST, _______, 
     _______, _______, _______, _______, _______, _______, _______, KC_KP_4, KC_KP_5, KC_KP_6, KC_PSLS, KC_PEQL,    
     _______, _______, _______, _______, _______, _______, _______, KC_KP_1, KC_KP_2, KC_KP_3, KC_PPLS, KC_PENT,    
